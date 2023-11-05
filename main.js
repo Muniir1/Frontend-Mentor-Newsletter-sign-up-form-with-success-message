9f6c942a1fe8991003335273d8b2780e5c40b975
Muniir1 <muniirm81@gmail.com>
2023-11-05 17:23:38 +0300

    updated

--- main.js
+++ main.js
@@ -5,31 +5,45 @@ const btn2 = document.getElementById("btn2");
 const form = document.querySelector("form");
 const success = document.querySelector(".success");
 
-form.addEventListener("submit", (e)=> {
-    e.preventDefault();
+let errorLabel; 
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     getValues();
-})
+});
 
 const getValues = () => {
     const inputValue = input.value.trim();
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (inputValue.match(mailformat) ){
-        container.classList.add("remove")
-        success.style.display = "block"
+    const email = input.value;
+    document.getElementById("email").innerHTML = email;
+
+    if (inputValue.match(mailformat)) {
+        container.classList.add("remove");
+        success.style.display = "block";
+    } else {
+        input.classList.add("error");
+
+
+        if (!errorLabel) {
+            errorLabel = document.createElement("label");
+            errorLabel.classList.add("error_label");
+            errorLabel.innerHTML = "Valid Email Required";
+            form.appendChild(errorLabel);
+        }
     }
-    else{
-        input.classList.add("error")
-        const errorLabel = document.createElement("label")
-        form.appendChild(errorLabel);
-        errorLabel.classList.add("error_label")
-        errorLabel.innerHTML = "Valid Email Required"
-    }
-}
+};
 
 const dismisMessage = () => {
-    success.style.display = "none"
-    container.classList.remove("remove")
-}
+    input.classList.remove("error");
+    success.style.display = "none";
+    container.classList.remove("remove");
+
+
+    if (errorLabel) {
+        errorLabel.remove();
+        errorLabel = null; 
+    }
+};
 
 btn2.addEventListener("click", dismisMessage);
